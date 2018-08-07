d732c28b91f74508dcd50b7816541af9943186ff
setzer22 <jsanchezfsms@gmail.com>
2018-08-07 09:44:47 +0200

    Added Minimize Others hotkey

--- contents/code/main.js
+++ contents/code/main.js
@@ -16,56 +16,99 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 
 function MiniStack() {
-	this.stack = [];
-	try {
-		var self = this;
-		workspace.clientRemoved.connect(function (client) {
-			self.removeClient(client);
-		});
-		workspace.clientMinimized.connect(function (client) {
-			self.addClient(client);
-		});
-		workspace.clientUnminimized.connect(function (client) {
-			self.removeClient(client);
-		});
-		registerShortcut("Unminimize last minimized Client",
-						 "Unminimize last minimized Client",
-						 "Meta+,",
-						function () {
-							self.pop();
-						});
-		registerShortcut("Unminimize all minimized clients",
-						 "Unminimize all minimized clients",
-						 "Meta+.",
-						 function () {
-							 for(i = 0; i < self.stack.length; i++) {
-								 self.pop();
-							 }
-						 });
-	} catch(err) {
-		print(err, "in ministack");
-	}
+	  this.stack = [];
+	  try {
+		    var self = this;
+		    workspace.clientRemoved.connect(function (client) {
+			      self.removeClient(client);
+		    });
+		    workspace.clientMinimized.connect(function (client) {
+			      self.addClient(client);
+		    });
+		    workspace.clientUnminimized.connect(function (client) {
+			      self.removeClient(client);
+		    });
+		    registerShortcut("MINSTACK: Unminimize last minimized Client",
+						             "MINSTACK: Unminimize last minimized Client",
+						             "Meta+,",
+						             function () {
+							               self.pop();
+						             });
+		    registerShortcut("MINSTACK: Unminimize all minimized clients",
+						             "MINSTACK: Unminimize all minimized clients",
+						             "Meta+.",
+						             function () {
+							               for(i = 0; i < self.stack.length; i++) {
+								                 self.pop();
+							               }
+						             });
+        registerShortcut("MINSTACK: Minimize all clients but selected",
+                         "MINSTACK: Minimize all clients but selected",
+                         "Meta+W",
+                         function() {
+                             // NOTE: This function was adapted from https://github.com/karakum/kwin-script-onlyOneActiveWindow/
+                             //  Copyright (C) 2015 Andrey Shertsinger <andrey@shertsinger.ru>
+                             //  Also distributed under the GNU GPL v2
+
+                             var client = workspace.activeClient;
+                             if (!client) return;
+
+	                           if (client.skipTaskbar || client.modal || client.transient){
+			                           return;
+	                           }
+	                           var clients = workspace.clientList();
+	                           var transients = [];
+	                           for(var i = 0; i < clients.length; i++) {
+		                             if(clients[i].transient){
+			                               transients[clients[i].transientFor]=1;
+		                             }
+	                           }
+	                           if(client.transient){
+		                             transients[client.transientFor]=1;
+	                           }
+	                           var actives=[];
+	                           var ai=0;
+	                           for(var i = 0; i < clients.length; i++) {
+		                             if(clients[i].minimizable && transients[clients[i]] != 1 &&
+				                            clients[i] != client &&
+                                    clients[i].screen == client.screen &&
+				                            clients[i].desktop == client.desktop &&
+				                            !( clients[i].skipTaskbar || clients[i].skipSwitcher || clients[i].skipPager || clients[i].transient || clients[i].modal )){
+			                               actives[ai]=clients[i];
+			                               ai++;
+		                             }
+	                           }
+	                           for(var i = 0; i < ai; i++) {
+		                             actives[i].minimized = true;
+	                           }
+	                           if(ai==1){
+		                             lastActive=actives[0];
+	                           }
+                         });
+	  } catch(err) {
+		    print(err, "in ministack");
+	  }
 }
-	
+
 MiniStack.prototype.addClient = function(client) {
-	this.stack.push(client);
+	  this.stack.push(client);
 };
 
 MiniStack.prototype.removeClient = function(client) {
-	for (i = 0; i < this.stack.length; i++) {
-		if (this.stack[i] == client) {
-			this.stack.splice(i,1);
-		}
-	}
+	  for (i = 0; i < this.stack.length; i++) {
+		    if (this.stack[i] == client) {
+			      this.stack.splice(i,1);
+		    }
+	  }
 };
 
 MiniStack.prototype.pop = function() {
-	if (this.stack.length > 0) {
-		var client = this.stack[this.stack.length - 1];
-		client.minimized = false;
-		client.desktop = workspace.currentDesktop;
-	} else {
-	}
+	  if (this.stack.length > 0) {
+		    var client = this.stack[this.stack.length - 1];
+		    client.minimized = false;
+		    client.desktop = workspace.currentDesktop;
+	  } else {
+	  }
 };
 
 m = new MiniStack();
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+plasmapkg2 --type kwinscript --remove ministack
+plasmapkg2 --type kwinscript -i /home/josep/Repositories/kwin-ministack/.
